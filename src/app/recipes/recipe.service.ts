20daab509c0990983d5b466585db95cd0a7e338c
Denislav Veizov <denislav.veizov@duosoft.net>
2020-05-15 16:57:13 +0300

    Initial project commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Tasty Schnitzel', 'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ]
+    ),
+    new Recipe('Big Far Burger', 'Other recipe', 'https://www.aladinfoods.bg/files/images/279/pileshki_burger_classic.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ]
+    ),
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+}
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,26 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const intAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, intAmount);
+    this.shoppingListService.addIngredient(newIngredient);
+  }
+}
